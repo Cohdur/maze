1ad412241c1277615cb2b9643d826710cda2a49d
Cohdur <starzecnorbert37@gmail.com>
2025-04-23 05:45:58 -0500

    start position
    Random start position assigned.

--- maze.cpp
+++ maze.cpp
@@ -1,28 +1,273 @@
-#include "graph_class.h"
 #include<vector>
+#include<iostream>
+#include<ctime>
+#include<random>
 
-class maze : public graph_class<char, int> // char = character/ board & int is edge weight
+//#include "graph_class.h"
+
+class maze //: public graph_class<char, int> // char = character/ board & int is edge weight
 {
 
     private:
 
     const static int size = 4;
-    char grid[4][4] =
+    const static int big_size = size * 4;
+
+    int start;
+    int exit;
+
+    
+    std::vector<std::vector<char>> grid
     {
         {'+', '+', '+', '+'},
-        {'|', ' ', ' ', '|'},
-        {'|', ' ', ' ', '|'},
+        {'+', ' ', ' ', '+'},
+        {'+', ' ', ' ', '+'},
         {'+', '+', '+', '+'}
     };
 
     std::vector<std::vector<char>> big_maze;
 
+    std::vector<int> testValues;
+    std::vector<int> testValues2;
+    public :
 
-    class position : public graph_class::Vertex
-    {
+        void createMaze()
+        {
+            //std::vector<int> Grid_choices = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // can be inlined for dynamics
+            //std::vector<int> wall_choices = {0, 1, 2, 3, 4, 7, 8, 11, 12, 13, 14, 15}; // utilized in switch
+            std::random_device rd;
+            std::mt19937 engine(rd());
+            std::uniform_int_distribution<> distrib(0, 15); //Grid_choices.size() - 1); 
+            int random_choice = distrib(engine);
+            //int random_choice = Grid_choices[distrib(engine)];
 
+            // Initialize big_maze with the correct size
+            big_maze.resize(big_size, std::vector<char>(big_size, ' '));
+            
+            // Fill big_maze with repeated copies of grid
+            for(int block_row = 0; block_row < 4; ++block_row)
+            {
+                for (int block_col = 0; block_col < 4; ++block_col)
+                {
+                    for (int i = 0; i < size; ++i)
+                    {
+                        for (int j = 0; j < size; ++j)
+                        {
+                            testValues.push_back(block_row * size + i);
+                            testValues2.push_back(block_col * size + j);
+                            big_maze[block_row * size + i][block_col * size + j] = grid[i][j];
+                        }
+                    }
+                }
+            }
+            //Just assigning a start which needs to be root/first vertex assigned for algorithm -> exit 'H'
+            switch(random_choice)
+            {
+                case 0 : 
+                {
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0, 15);
+                    int random_choice2 = distrib(engine);
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
 
-    };
+                case 1 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                case 2 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                case 3 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                case 4 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 5 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 6 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 7 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 8 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 9 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 10 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 11 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 12 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                case 13 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                case 14 :
+                {
+                    std::vector<int> wall_choices{0, 15};
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,1);
+                    int random_choice2 = wall_choices[distrib(engine)];
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+                case 15 :
+                {
+                    std::random_device rd;
+                    std::mt19937 engine(rd());
+                    std::uniform_int_distribution<> distrib(0,15);
+                    int random_choice2 = distrib(engine);
+                    start = big_maze.at(random_choice).at(random_choice2) = 'A';
+                }
+                break;
+
+                default :
+                std::cout << "it didn't work " << std::endl;
+                break;
+            }
+        }
+
+        void output()
+        {
+            for(auto i : big_maze)
+            {
+                for(auto x : i)
+                {
+                    std::cout << x << ' ';
+                }
+                std::cout << std::endl;
+            }
+
+        }
+
+        
+
+/*
+class position : public graph_class::Vertex
+{
     
 
 };
+*/
+    
+
+};
+
+
+using namespace std;
+
+int main()
+{
+    maze game;
+
+    game.createMaze();
+
+    game.output();
+}
